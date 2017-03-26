d1e6e5593ae614187cc2dfd57ee526c17de6450d
LinkedDestiny <simonarthur2012@gmail.com>
2017-03-27 00:04:10 +0800

    add cache init

--- app/server/HttpServer.php
+++ app/server/HttpServer.php
@@ -12,6 +12,7 @@ use base\Entrance;
 use base\framework\Route;
 use base\protocol\Request;
 use base\socket\BaseCallback;
+use base\socket\SwooleServer;
 use core\common\Globals;
 use core\component\cache\CacheLoader;
 use core\component\config\Config;
@@ -28,12 +29,20 @@ class HttpServer extends BaseCallback
         // 初始化连接池
         PoolManager::getInstance()->init('mysql_master');
         PoolManager::getInstance()->init('redis_master');
+
+        /**
+         * 初始化内存缓存
+         */
+        $cache_config = Config::getField('component', 'cache');
+        CacheLoader::getInstance()->init(Entrance::$rootPath . $cache_config['cache_path'],
+            $cache_config['cache_path']);
     }
 
     public function before_start()
     {
         // 打开内存Cache进程
         $this->open_cache_process(function(){
+            Globals::$server = SwooleServer::getInstance()->getServer();
             Globals::setProcessName(Config::getField('project', 'project_name') . 'cache process');
             $cache_config = Config::getField('component', 'cache');
 
--- config/debug/component.php
+++ config/debug/component.php
@@ -35,7 +35,7 @@ return [
          * 内存缓存组件
          */
         'cache' => [
-            'open_cache'    => false,            // 是否开启内存缓存模块
+            'open_cache'    => true,            // 是否开启内存缓存模块
             'cache_tick'    => 10000,           // 缓存刷新的间隔时间,单位为ms
             'cache_path'    => '/app/cache/', // 缓存更新文件的路径
         ]
